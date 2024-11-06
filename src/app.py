25ff096a3e41136e8ce840fc4e75ecdcb1d3bfc9
JulianCM21 <150470789+JulianCM21@users.noreply.github.com>
2024-11-06 01:20:39 +0000

    Banking Marketing Campaign Julian Cerro

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
 from utils import db_connect
 engine = db_connect()
 
-# your code here
+URL = "https://raw.githubusercontent.com/4GeeksAcademy/logistic-regression-project-tutorial/main/bank-marketing-campaign-data.csv"
